1b71a6d0222b224e8ed05e8f962a0dec599ea0b2
Paul Rozhkin <blackiiifox@gmail.com>
2021-11-28 17:27:36 +0300

    add minio integration for files

---
+++ src/backend/ApiGateways/Web.HttpAggregator/aggregator/Web.HttpAggregator/Controllers/ResourcesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Minio;
+
+namespace Web.HttpAggregator.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ResourcesController : ControllerBase
+    {
+        private readonly MinioClient _minioClient;
+
+        public ResourcesController(MinioClient minioClient)
+        {
+            _minioClient = minioClient;
+        }
+
+        [HttpPost]
+        public async Task UploadFiles(List<IFormFile> files)
+        {
+            try
+            {
+                var file = files.First();
+                var inputStream = file.OpenReadStream();
+
+                var bucketName = "files";
+                var found = await _minioClient.BucketExistsAsync(bucketName);
+
+                if (!found)
+                {
+                    await _minioClient.MakeBucketAsync(bucketName);
+                }
+
+                // Upload a file to bucket.
+                await _minioClient.PutObjectAsync(bucketName, Guid.NewGuid().ToString(), inputStream,
+                    inputStream.Length, file.ContentType);
+            }
+            catch (Exception ex)
+            {
+                throw new($"Error on file processing - {ex.Message}");
+            }
+        }
+    }
+}
